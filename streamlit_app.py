0a38cdcf1352d0c603e46ec86b7e255c479f97b8
JuanCJC <juan.ibanez@bewe.io>
2025-01-14 18:00:56 -0500

    Adding interview-agent

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,65 @@
+import requests
 import streamlit as st
-from openai import OpenAI
+import random
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+URL = "https://backendbeweliteqa.bewe.co/api/v1/llm/interview"
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+def interviewer_agent() -> str:
+	print("interviewer_agent executed")
+	st.title("Helper stream lit")
+	st.write("This is a helper streamlit chat command")
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+	# Initialize thread_id in session state if it doesn't exist
+	if "thread_id" not in st.session_state:
+		st.session_state.thread_id = str(random.randint(1, 1000000))
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+	account_id = st.sidebar.text_input('Account ID', value="234234234234")
+	thread_id = st.sidebar.text_input('Thread ID', value=st.session_state.thread_id)
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+	language = st.sidebar.selectbox(
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+		'Language',
+		('spanish', 'english', 'portuguese')
+	)
+	account_type = st.sidebar.selectbox(
+			'Type',
+			('barbershops', 'restaurants', 'hotels', 'shopping', 'other')
+	)
+	if account_type == 'other':
+		account_type = st.sidebar.text_input('Other')
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+	# Initialize chat history
+	if "messages" not in st.session_state:
+		st.session_state.messages = []
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+	# Display chat messages from history on app rerun
+	for message in st.session_state.messages:
+		with st.chat_message(message["role"]):
+			st.markdown(message["content"])
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+	# Accept user input
+	if prompt := st.chat_input("What is up?"):
+		# Add user message to chat history
+		st.session_state.messages.append({"role": "user", "content": prompt})
+		# Display user message in chat message container
+		with st.chat_message("user"):
+			st.markdown(prompt)
+		print(prompt)
+		# Display assistant response in chat message container
+		with st.chat_message("assistant"):
+			message_placeholder = st.empty()
+			with st.spinner("Thinking..."):
+				response = requests.post(URL, json={
+					"answer": prompt,
+					"thread_id": thread_id,
+					"account_id": account_id,
+					"type": account_type,
+					"language": language
+				})
+				full_response = response.json().get("response", "")
+				message_placeholder.markdown(full_response)
+				st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
+if __name__ == "__main__":
+	interviewer_agent()
